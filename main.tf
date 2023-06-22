73a0369290afd3d00220407ad5edeb38eef9eb61
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2023-06-22 19:14:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIATFXGWHCNBDVCBS7H"
-  secret_key = "5V5v90/9DEr+I1AGryER7wQFtmRBs2Qe7ryMR1YJ"
+  access_key = ""
+  secret_key = ""
 }
 
 resource "aws_s3_bucket" "example" {
